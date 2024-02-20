9875589e85f12f93df631f9fbe0f11e0c8aa87f0
sikicode <siqicao811@gmail.com>
2024-02-20 12:45:27 -0800

    setup routes

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,6 +1,7 @@
 import express from 'express';
 import { config } from 'dotenv';
 import morgan from 'morgan';
+import appRouter from "./routes/index.js";
 
 config();
 const app = express();
@@ -9,6 +10,6 @@ const app = express();
 app.use(express.json());
 app.use(morgan("dev"));
 
-app.use("api/v1");
+app.use("api/v1", appRouter);
 
 export default app;
