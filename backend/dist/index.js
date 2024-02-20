11a50c02592d23bfad015292cc7c8c3987ac4767
sikicode <siqicao811@gmail.com>
2024-02-19 17:37:20 -0800

    connect to MongoDB Database

--- backend/dist/index.js
+++ backend/dist/index.js
@@ -1,3 +1,10 @@
-console.log("Hello World");
-export {};
+import app from "./app.js";
+import { connectToDatabase } from "./db/connection.js";
+// connections and listeners
+const PORT = process.env.PORT || 5000;
+connectToDatabase()
+    .then(() => {
+    app.listen(PORT, () => console.log("Server Open & Connected To Database"));
+})
+    .catch((err) => console.log(err));
 //# sourceMappingURL=index.js.map
---
+++ backend/src/app.ts
@@ -0,0 +1,10 @@
+import express from 'express';
+import { config } from 'dotenv';
+
+config();
+const app = express();
+
+// middlewares
+app.use(express.json());
+
+export default app;
