46cdb3629281449995377041171af766f6a431cb
sikicode <siqicao811@gmail.com>
2024-02-19 18:25:31 -0800

    add morgan

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,10 +1,14 @@
 import express from 'express';
 import { config } from 'dotenv';
+import morgan from 'morgan';
 
 config();
 const app = express();
 
 // middlewares
 app.use(express.json());
+app.use(morgan("dev"));
+
+app.use("api/v1");
 
 export default app;
