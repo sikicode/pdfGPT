510a8e7200176f0c029d10bff5d675d46a563c9f
sikicode <siqicao811@gmail.com>
2024-02-25 22:54:35 -0800

    introduce cookie parser and cookie secret

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -2,12 +2,14 @@ import express from 'express';
 import { config } from 'dotenv';
 import morgan from 'morgan';
 import appRouter from "./routes/index.js";
+import cookieParser from "cookie-parser";
 
 config();
 const app = express();
 
 // middlewares
 app.use(express.json());
+app.use(cookieParser(process.env.COOKIE_SECRET));
 app.use(morgan("dev"));
 
 app.use("/api/v1", appRouter);
