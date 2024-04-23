ba29b43c2762b7abd2312570cf518e786f6020d0
Siqi Cao <siqicao811@gmail.com>
2024-04-23 14:50:52 -0700

    add buttons on home with style

--- frontend/src/components/shared/NavigationLink.tsx
+++ frontend/src/components/shared/NavigationLink.tsx
@@ -10,6 +10,7 @@ type Props = {
 
 const NavigationLink = (props: Props) => {
     return <Link
+        className="nav-link"
         to={props.to}
         style={{ background: props.bg, color: props.textColor }}
     >
