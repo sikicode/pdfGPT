f066f7df3defb7b357d3d456ce05b2020651c93c
Siqi Cao <siqicao811@gmail.com>
2024-04-23 18:06:58 -0700

    add backend token verification, if token is not verified reject, otherwise send credentials to frontend

--- backend/src/controllers/user-controllers.ts
+++ backend/src/controllers/user-controllers.ts
@@ -98,3 +98,25 @@ export const userLogin = async (
         return res.status(200).json({ message: "ERROR", cause: error.message });
     }
 };
+
+export const verifyUser = async (
+    req: Request,
+    res: Response,
+    next: NextFunction,
+) => {
+    try {
+        const user = await User.findById({ email:res.locals.jwtData.email });
+        if(!user) {
+            return res.status(401).send("User not registered OR Token malfunctioned");
+        }
+        console.log(user._id.toString(), res.locals.jwtData.id);
+        if(user._id.toString() !== res.locals.jwtData.id) {
+            return res.status(401).send("Permissions didn't match");
+        }
+        return res.status(200).json({message: "OK",  name:user.name,
+            email:user.email});
+    } catch (error) {
+        console.log(error);
+        return res.status(200).json({message: "ERROR", cause: error.message});
+    }
+};
--- backend/src/utils/token-manager.ts
+++ backend/src/utils/token-manager.ts
@@ -1,4 +1,6 @@
+import { Request, Response, NextFunction } from "express";
 import jwt from 'jsonwebtoken';
+import {COOKIE_NAME} from "./constants.js";
 
 export const createToken = (id: string, email: string, expiresIn: string) => {
     const payload = { id, email };
@@ -6,4 +8,25 @@ export const createToken = (id: string, email: string, expiresIn: string) => {
         expiresIn,
     });
     return token;
+};
+
+export const verifyToken = async (req:Request, res:Response, next:NextFunction) => {
+    const token = req.signedCookies[`${COOKIE_NAME}`];
+    //console.log(token);
+    if (!token || token.trim() === "") {    // if token is null
+        return res.status(401).json({message:"Token Not Received"});
+    }
+    return new Promise<void>((resolve, reject) => {
+        return jwt.verify(token, process.env.JWT_SECRET, (err, success) => {
+            if(err) {
+                reject(err.message);
+                return res.status(401).json({message:"Token Expired"});
+            } else {
+                console.log("Token Verification Successful");
+                resolve();
+                res.locals.jwtData = success;
+                return next();
+            }
+        })
+    });
 };
